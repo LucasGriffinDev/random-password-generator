26d3c505b522217aa8956014592b4768cf5f596b
Lucas Griffin <66416659+SuburbanTurnip@users.noreply.github.com>
2021-08-17 11:33:30 +1000

    linked password length slider with input field
    changing one changes the other.:
    
    @@ -0,0 +1,12 @@
    const CharacterAmountRange = document.getElementById('CharacterAmountRange')
    const characterAmountNumber = document.getElementById('characterAmountNumber')
    
    characterAmountNumber.addEventListener('input', syncCharacterAmount)
    characterAmountRange.addEventListener('input', syncCharacterAmount)
    
    function syncCharacterAmount(e) {
        const value = e.target.value
        characterAmountNumber.value = value
        characterAmountRange.value = value
    }

---
+++ script.js
@@ -0,0 +1,12 @@
+const CharacterAmountRange = document.getElementById('CharacterAmountRange')
+const characterAmountNumber = document.getElementById('characterAmountNumber')
+
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
+
+function syncCharacterAmount(e) {
+    const value = e.target.value
+    characterAmountNumber.value = value
+    characterAmountRange.value = value
+}
+
